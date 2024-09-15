2f3f26f930910ed111660998dc4023ad6071354f
taitu <taitu.dev@gmail.com>
2024-09-15 11:20:07 +0300

    test symbols

--- tests/symbols.spec.ts
+++ tests/symbols.spec.ts
@@ -2,7 +2,7 @@ import {JSDOM} from 'jsdom';
 import {KeyboardSimulator} from 'keyboard-simulator';
 import {it, beforeAll, beforeEach, afterEach, Mock, describe, expect} from 'vitest';
 import {hotkey, Hotkey} from '../src';
-import {spyFn} from './utils';
+import {spies, spyFn} from './utils';
 
 describe('Symbols', () => {
 	let doc: Document | undefined;
@@ -28,211 +28,210 @@ describe('Symbols', () => {
 		spy.mockClear();
 	});
 
-	it('Square Brackets', () => {
+	it('Open / Close', () => {
+		const [spy1, spy2, spy3, spy4, spy5, spy6, spy7, spy8] = spies(8);
+
 		hk.bindKeys({
-			'[': spy,
-			']': spy,
+			'[': spy1,
+			']': spy2,
+			'{': spy3,
+			'}': spy4,
+			'(': spy5,
+			')': spy6,
+			'<': spy7,
+			'>': spy8,
 		});
 
-		simulate.keyPress('BracketLeft');
-		expect(spy).toHaveBeenCalledTimes(1);
-		simulate.keyPress('BracketRight');
-		expect(spy).toHaveBeenCalledTimes(2);
-	});
-
-	it('Curly Braces', () => {
-		hk.bindKeys({
-			'{': spy,
-			'}': spy,
-		});
+		simulate.keyDown('BracketLeft');
+		expect(spy1).toHaveBeenCalledOnce();
+		simulate.releaseAll();
+		simulate.keyDown('BracketRight');
+		expect(spy2).toHaveBeenCalledOnce();
+		simulate.releaseAll();
 
 		simulate.keyDown('Shift', 'BracketLeft');
-		expect(spy).toHaveBeenCalledTimes(1);
+		expect(spy3).toHaveBeenCalledOnce();
 		simulate.releaseAll();
 		simulate.keyDown('Shift', 'BracketRight');
-		expect(spy).toHaveBeenCalledTimes(2);
+		expect(spy4).toHaveBeenCalledOnce();
 		simulate.releaseAll();
-	});
-
-	it('Parentheses ', () => {
-		hk.bindKeys({
-			'(': spy,
-			')': spy,
-		});
 
 		simulate.keyDown('Shift', 'Digit9');
-		expect(spy).toHaveBeenCalledTimes(1);
+		expect(spy5).toHaveBeenCalledOnce();
 		simulate.releaseAll();
 		simulate.keyDown('Shift', 'Digit0');
-		expect(spy).toHaveBeenCalledTimes(2);
+		expect(spy6).toHaveBeenCalledOnce();
 		simulate.releaseAll();
-	});
-
-	it('Greater & Lower than', () => {
-		hk.bindKeys({
-			'<': spy,
-			'>': spy,
-		});
 
 		simulate.keyDown('Shift', 'Comma');
-		expect(spy).toHaveBeenCalledTimes(1);
+		expect(spy7).toHaveBeenCalledOnce();
 		simulate.releaseAll();
 		simulate.keyDown('Shift', 'Period');
-		expect(spy).toHaveBeenCalledTimes(2);
+		expect(spy8).toHaveBeenCalledOnce();
 		simulate.releaseAll();
 	});
 
-	it('Period & Comma', () => {
+	it('Punctuation & Text', () => {
+		const [spy1, spy2, spy3, spy4, spy5, spy6, spy7, spy8] = spies(8);
+
 		hk.bindKeys({
-			'.': spy,
-			',': spy,
+			'.': spy1,
+			',': spy2,
+			'\\': spy3,
+			'\'': spy4,
+			'"': spy5,
+			'`': spy6,
+			';': spy7,
+			':': spy8,
 		});
 
-		simulate.keyPress('Period');
-		expect(spy).toHaveBeenCalledTimes(1);
-		simulate.keyPress('NumpadDecimal');
-		expect(spy).toHaveBeenCalledTimes(2);
-		simulate.keyPress('Comma');
-		expect(spy).toHaveBeenCalledTimes(3);
-	});
-
-	it('Slash & Backslash', () => {
-		hk.bindKeys({
-			'/': spy,
-			'\\': spy,
-		});
-
-		simulate.keyPress('Slash');
-		expect(spy).toHaveBeenCalledTimes(1);
-		simulate.keyPress('Backslash');
-		expect(spy).toHaveBeenCalledTimes(2);
-	});
-
-	it('Quotes', () => {
-		hk.bindKeys({
-			'\'': spy,
-			'"': spy,
-			'`': spy,
-		});
-
-		simulate.keyPress('SingleQuote');
-		expect(spy).toHaveBeenCalledTimes(1);
-		simulate.keyDown('Shift', 'Quote');
-		expect(spy).toHaveBeenCalledTimes(2);
+		simulate.keyDown('Period');
+		expect(spy1).toHaveBeenCalledOnce();
 		simulate.releaseAll();
-		simulate.keyPress('Backquote');
-		expect(spy).toHaveBeenCalledTimes(3);
-	});
 
-	it('Colon & Semicolon', () => {
-		hk.bindKeys({
-			':': spy,
-			';': spy,
-		});
+		simulate.keyDown('Decimal');
+		expect(spy1).toHaveBeenCalledTimes(2);
+		simulate.releaseAll();
 
-		simulate.keyPress('Semicolon');
-		expect(spy).toHaveBeenCalledTimes(1);
+		simulate.keyDown('Comma');
+		expect(spy2).toHaveBeenCalledOnce();
+		simulate.releaseAll();
+
+		simulate.keyDown('Backslash');
+		expect(spy3).toHaveBeenCalledOnce();
+		simulate.releaseAll();
+
+		simulate.keyDown('SingleQuote');
+		expect(spy4).toHaveBeenCalledOnce();
+		simulate.releaseAll();
+
+		simulate.keyDown('Shift', 'SingleQuote');
+		expect(spy5).toHaveBeenCalledOnce();
+		simulate.releaseAll();
+
+		simulate.keyDown('Backquote');
+		expect(spy6).toHaveBeenCalledOnce();
+		simulate.releaseAll();
+
+		simulate.keyDown('Semicolon');
+		expect(spy7).toHaveBeenCalledOnce();
+		simulate.releaseAll();
+
 		simulate.keyDown('Shift', 'Semicolon');
-		expect(spy).toHaveBeenCalledTimes(2);
-	});
-
-	it('Question & Excalamtion Marks', () => {
-		hk.bindKeys({
-			'?': spy,
-			'!': spy,
-		});
-
-		simulate.keyDown('Shift', 'Slash');
-		expect(spy).toHaveBeenCalledTimes(1);
-		simulate.releaseAll();
-
-		simulate.keyDown('Shift', 'Digit1');
-		expect(spy).toHaveBeenCalledTimes(2);
+		expect(spy8).toHaveBeenCalledOnce();
 		simulate.releaseAll();
 	});
 
-	it('Math Symbols', () => {
+	it('Calculator Symbols', () => {
+		const [spy1, spy2, spy3, spy4, spy5] = spies(5);
+
 		hk.bindKeys({
-			'+': spy,
-			'-': spy,
-			'*': spy,
-			'/': spy,
-			'=': spy,
+			'+': spy1,
+			'-': spy2,
+			'*': spy3,
+			'/': spy4,
+			'=': spy5,
 		});
 
-		simulate.keyPress('NumpadAdd');
-		expect(spy).toHaveBeenCalledTimes(1);
+		simulate.keyDown('NumpadAdd');
+		expect(spy1).toHaveBeenCalledOnce();
+		simulate.releaseAll();
+
 		simulate.keyDown('Shift', 'Equal');
-		expect(spy).toHaveBeenCalledTimes(2);
+		expect(spy1).toHaveBeenCalledTimes(2);
 		simulate.releaseAll();
 
-		simulate.keyPress('NumpadSubtract');
-		expect(spy).toHaveBeenCalledTimes(3);
-		simulate.keyPress('Minus');
-		expect(spy).toHaveBeenCalledTimes(4);
+		simulate.keyDown('NumpadSubtract');
+		expect(spy2).toHaveBeenCalledOnce();
+		simulate.releaseAll();
 
-		simulate.keyPress('NumpadMultiply');
-		expect(spy).toHaveBeenCalledTimes(5);
+		simulate.keyDown('Minus');
+		expect(spy2).toHaveBeenCalledTimes(2);
+		simulate.releaseAll();
+
+		simulate.keyDown('NumpadMultiply');
+		expect(spy3).toHaveBeenCalledOnce();
+		simulate.releaseAll();
+
 		simulate.keyDown('Shift', 'Digit8');
-		expect(spy).toHaveBeenCalledTimes(6);
+		expect(spy3).toHaveBeenCalledTimes(2);
 		simulate.releaseAll();
 
-		simulate.keyPress('NumpadDivide');
-		expect(spy).toHaveBeenCalledTimes(7);
-		simulate.keyPress('Slash');
-		expect(spy).toHaveBeenCalledTimes(8);
+		simulate.keyDown('NumpadDivide');
+		expect(spy4).toHaveBeenCalledOnce();
+		simulate.releaseAll();
 
-		simulate.keyPress('Equal');
-		expect(spy).toHaveBeenCalledTimes(9);
+		simulate.keyDown('Slash');
+		expect(spy4).toHaveBeenCalledTimes(2);
+		simulate.releaseAll();
+
+		simulate.keyDown('Equal');
+		expect(spy5).toHaveBeenCalledOnce();
+		simulate.releaseAll();
 	});
 
 	it('Other Shift Symbols', () => {
+		const [
+			spy1, spy2, spy3, spy4, spy5, spy6,
+			spy7, spy8, spy9, spy10, spy11,
+		] = spies(11);
+
 		hk.bindKeys({
-			'~': spy,
-			'@': spy,
-			'#': spy,
-			'$': spy,
-			'%': spy,
-			'^': spy,
-			'&': spy,
-			'_': spy,
-			'|': spy,
+			'~': spy1,
+			'!': spy2,
+			'@': spy3,
+			'#': spy4,
+			'$': spy5,
+			'%': spy6,
+			'^': spy7,
+			'&': spy8,
+			'_': spy9,
+			'|': spy10,
+			'?': spy11,
 		});
 
 		simulate.keyDown('Shift', 'Backquote');
-		expect(spy).toHaveBeenCalledTimes(1);
+		expect(spy1).toHaveBeenCalledOnce();
+		simulate.releaseAll();
+
+		simulate.keyDown('Shift', 'Digit1');
+		expect(spy2).toHaveBeenCalledOnce();
 		simulate.releaseAll();
 
 		simulate.keyDown('Shift', 'Digit2');
-		expect(spy).toHaveBeenCalledTimes(2);
+		expect(spy3).toHaveBeenCalledOnce();
 		simulate.releaseAll();
 
 		simulate.keyDown('Shift', 'Digit3');
-		expect(spy).toHaveBeenCalledTimes(3);
+		expect(spy4).toHaveBeenCalledOnce();
 		simulate.releaseAll();
 
 		simulate.keyDown('Shift', 'Digit4');
-		expect(spy).toHaveBeenCalledTimes(4);
+		expect(spy5).toHaveBeenCalledOnce();
 		simulate.releaseAll();
 
 		simulate.keyDown('Shift', 'Digit5');
-		expect(spy).toHaveBeenCalledTimes(5);
+		expect(spy6).toHaveBeenCalledOnce();
 		simulate.releaseAll();
 
 		simulate.keyDown('Shift', 'Digit6');
-		expect(spy).toHaveBeenCalledTimes(6);
+		expect(spy7).toHaveBeenCalledOnce();
 		simulate.releaseAll();
 
 		simulate.keyDown('Shift', 'Digit7');
-		expect(spy).toHaveBeenCalledTimes(7);
+		expect(spy8).toHaveBeenCalledOnce();
 		simulate.releaseAll();
 
 		simulate.keyDown('Shift', 'Minus');
-		expect(spy).toHaveBeenCalledTimes(8);
+		expect(spy9).toHaveBeenCalledOnce();
 		simulate.releaseAll();
 
 		simulate.keyDown('Shift', 'Backslash');
-		expect(spy).toHaveBeenCalledTimes(9);
+		expect(spy10).toHaveBeenCalledOnce();
+		simulate.releaseAll();
+
+		simulate.keyDown('Shift', 'Slash');
+		expect(spy11).toHaveBeenCalledOnce();
 		simulate.releaseAll();
 	});
 
@@ -253,24 +252,29 @@ describe('Symbols', () => {
 			expect(spy).toHaveBeenCalledTimes(1);
 			simulate.releaseAll();
 
-			simulate.keyPress('Minus');
+			simulate.keyDown('Minus');
 			expect(spy).toHaveBeenCalledTimes(2);
+			simulate.releaseAll();
 
-			simulate.keyPress('Space');
+			simulate.keyDown('Space');
 			expect(spy).toHaveBeenCalledTimes(3);
+			simulate.releaseAll();
 
-			simulate.keyPress('Backquote');
+			simulate.keyDown('Backquote');
 			expect(spy).toHaveBeenCalledTimes(4);
+			simulate.releaseAll();
 
-			simulate.keyPress('Quote');
+			simulate.keyDown('Quote');
 			expect(spy).toHaveBeenCalledTimes(5);
+			simulate.releaseAll();
 
 			simulate.keyDown('Shift', 'Quote');
 			expect(spy).toHaveBeenCalledTimes(6);
 			simulate.releaseAll();
 
-			simulate.keyPress('Backslash');
+			simulate.keyDown('Backslash');
 			expect(spy).toHaveBeenCalledTimes(7);
+			simulate.releaseAll();
 
 			simulate.keyDown('Shift', 'Minus');
 			expect(spy).toHaveBeenCalledTimes(8);
@@ -288,11 +292,13 @@ describe('Symbols', () => {
 			expect(spy).toHaveBeenCalledTimes(1);
 			simulate.releaseAll();
 
-			simulate.keyPress('Minus');
+			simulate.keyDown('Minus');
 			expect(spy).toHaveBeenCalledTimes(2);
+			simulate.releaseAll();
 
-			simulate.keyPress('Space');
+			simulate.keyDown('Space');
 			expect(spy).toHaveBeenCalledTimes(3);
+			simulate.releaseAll();
 		});
 
 		it('With Modifiers', () => {
